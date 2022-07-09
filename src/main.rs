165138bf96ab4bdea82c21d1f52d8a5a297b05d9
hans <hanshuai.w27@bytedance.com>
2022-07-09 18:47:43 +0800

    完善逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,68 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::str;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        match stream {
+            // 当Result 枚举类型匹配Ok时
+            Ok(stream) => {
+                // 如果链接成功，开启一个新的线程处理（这里没有使用线程池，并不严谨）
+                thread::spawn(move|| {
+                    // 将处理请求逻辑简单封装
+                    handle_connection(stream);
+                });
+            }
+            // 当Result 枚举匹配错误时
+            Err(e) => { 
+                // 如果链接错误，提示错误信息并停止运行
+                panic!("Error is {:?}", e) 
+            }
+        }
+    }
+}
 
-        println!("Connection established!");
+// 处理客户端链接事件
+/**
+* @param stream: TcpStream  客户端传入的流
+*/
+fn handle_connection(mut stream: TcpStream) {
+    // 定义一个存储用的数组，会不断的填充数据进来，需要声明为可变的
+    let mut buffer = [0; 1024];
+    // 需要持续接收客户端的输入并echo回去，所以需要一个loop
+    loop {
+        // 读取输入
+        let read_size = stream.read(&mut buffer).expect("读取错误！");
+
+        // 如果没输入需要退出该次链接
+        if read_size == 0 {
+            break;
+        }
+        // 做一个错误处理，这里只要输入内容不是字符串我们就认为是错误
+        match str::from_utf8(&buffer[..read_size]) {
+            // 如果转换成功返回字符串值。
+            Ok(v) => {
+                println!("debug log content is {:?}", v);
+                if v.starts_with("exit") {
+                    // 输出终止前的消息。
+                    stream.write(b"bye.\n").unwrap();
+                    break;
+                } else {
+                // 给客户端显示输入内容
+                    stream.write(v.as_bytes()).unwrap();
+                }
+            },
+            // 遇到转换错误输出错误信息，终止程序运行。
+            Err(_) => {
+                // 给客户端显示遇到错误
+                stream.write(b"Error!").unwrap();
+                continue;
+            },
+        };
     }
 }
